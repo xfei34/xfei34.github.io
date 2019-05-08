ee476b242044c5b3b3bf3bc1910fd62b7944ad37
xfei34 <xiaoshufei@gatech.edu>
2019-05-08 17:02:33 -0400

    ajax update

---
+++ resume/app.js
@@ -0,0 +1,5 @@
+$(()=>{
+    $("#pdf").click(()=>{
+        window.open("https://v2018.api2pdf.com/chrome/url?url=https://wisdomkim.github.io/resume2/&apikey=a8167069-cd4b-4f07-9fc8-cc928ea96cc2")
+    })
+})  
